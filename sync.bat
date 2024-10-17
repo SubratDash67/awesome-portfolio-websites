63360ae191e7865342d7744e6f535bc49955c9ad
SubratDash67 <subratdash2022@gmail.com>
2024-10-17 10:18:53 +0530

    Automated sync commit

--- sync.bat
+++ sync.bat
@@ -1,7 +1,7 @@
 @echo off
 cd "C:\Users\KIIT\Desktop\awesome-portfolio-websites"
-git pull origin main
+git pull origin master
 git add .
 git commit -m "Automated sync commit"
-git push origin main
+git push origin master
 pause
